edf164137ec0b510309aee7d0eb78a8a67cabfb5
Ljunh23 <jun026919@gmail.com>
2026-07-19 20:25:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -2,12 +2,10 @@ def solution(n):
     answer = 0
     sqrt = n ** (1/2)
     num = int(sqrt)
-    if sqrt > 0:
-        if sqrt == num:
-            answer = (num+1)**2
-        else:
-            answer = -1
+    if sqrt == num:
+        answer = (num+1)**2
     else:
         answer = -1
+
             
     return answer
